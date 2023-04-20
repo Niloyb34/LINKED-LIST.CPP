17a4307489add5d4a2d0907c928669718ed00fae
Niloy Barua <98206229+Niloyb34@users.noreply.github.com>
2023-04-20 23:16:48 +0600

    Create ~NODE DESTRUCTOR FUNCTION.cpp

---
+++ ~NODE DESTRUCTOR FUNCTION.cpp
@@ -0,0 +1,7 @@
+The ~Node() function is the destructor of the Node class, which is called automatically when a node object is destroyed or goes out of scope. In this code, the destructor is used to free the dynamically allocated memory used by the linked list nodes.
+
+When the destructor is called, it first stores the data of the current node in the value variable. Then, it checks if the next pointer of the current node is pointing to another node or not. If the next pointer is not NULL, it recursively calls the destructor for the next node using the delete operator. This recursive call continues until it reaches the last node in the linked list, which has a NULL next pointer.
+
+Once the recursive call is completed and the last node is deleted, the destructor sets the next pointer of the current node to NULL and frees the memory allocated to the current node using the delete operator. Finally, it prints a message indicating that the memory for the current node has been freed.
+
+So, the ~Node() function is executed automatically by the system whenever a node object is destroyed or goes out of scope.
